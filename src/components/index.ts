e97d749b0376363635bf917e660130988f18dc8c
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2024-10-18 16:15:57 -0500

    create component index

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import App from "./App/App";
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import SelectInput from "./SelectInput/SelectInput";
+import TextareaInput from "./TextareaInput/TextareaInput";
+
+export { App, Button, Footer, Header, SelectInput, TextareaInput };
