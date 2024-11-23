a7e1c3e285e8a56b65c5ef3d07e7ce36165be224
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2024-11-23 09:03:12 -0500

    update positioning and vertical padding of header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ type HeaderProps = {
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <header className="w-full py-4">
+    <header className="absolute top-0 w-full py-4 lg:py-2 xl:py-12">
       <h1 className="text-2xl lg:text-4xl xl:text-6xl tracking-wide font-bold text-center text-blue-600">
         {children}
       </h1>
